f331b7f34d954cb40c6e9f97234db006f08ea12d
LucaHLR <mastrantonio.luca.91@icloud.com>
2021-09-17 16:01:08 +0200

    Push esercizio

---
+++ src/components/Hero.vue
@@ -0,0 +1,13 @@
+<template>
+    <img src="../assets/Superman_Profile_2.jpg" alt="Superman">
+</template>
+
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>La Mia Prima App Con Vue</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Title',
+}
+</script>
+
+<style lang="scss" scoped>
+    h1 {
+        text-decoration: underline;
+    }
+</style>>
+    
